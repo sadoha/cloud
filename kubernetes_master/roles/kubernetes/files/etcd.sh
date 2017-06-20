451ded2343ff3e2b539e08f8030b5242f06e02f5
root <andrii.sadovskyi@gmail.com>
2017-06-21 01:28:39 +0300

    cloud data

---
+++ kubernetes_master/roles/kubernetes/files/etcd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+checkKeys="`etcdctl ls /kube-centos/network 2&> /tmp/etcdctl-tmp.log`" 
+
+if [ "`cat /tmp/etcdctl-tmp.log | grep subnets`" != "" ] ; then 
+  etcdctl mkdir /kube-centos/network
+  etcdctl mk /kube-centos/network/config "{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }"
+fi
+
+rm /tmp/etcdctl-tmp.log 
