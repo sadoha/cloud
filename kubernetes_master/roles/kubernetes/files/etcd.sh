ac9504378c59d15f4a7672167844796b3d41701a
root <andrii.sadovskyi@gmail.com>
2017-06-22 11:03:08 +0300

    update kubernetes_master/roles/kubernetes/files/etcd.sh

--- kubernetes_master/roles/kubernetes/files/etcd.sh
+++ kubernetes_master/roles/kubernetes/files/etcd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-checkKeys="`etcdctl ls /kube-centos/network 2&> /tmp/etcdctl-tmp.log`" 
+etcdctl ls /kube-centos/network 2&> /tmp/etcdctl-tmp.log 
 
-if [ "`cat /tmp/etcdctl-tmp.log | grep subnets`" != "" ] ; then 
+if [ "`cat /tmp/etcdctl-tmp.log | grep Error:`" != "" ] ; then 
   etcdctl mkdir /kube-centos/network
   etcdctl mk /kube-centos/network/config "{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }"
 fi
 
-rm /tmp/etcdctl-tmp.log 
+#rm /tmp/etcdctl-tmp.log 
