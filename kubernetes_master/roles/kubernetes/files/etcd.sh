abce2399227906f870ac71ba5078980ffea4ac3b
root <andrii.sadovskyi@gmail.com>
2017-06-26 16:55:34 +0300

    Script etcd.sh has been updated.

--- kubernetes_master/roles/kubernetes/files/etcd.sh
+++ kubernetes_master/roles/kubernetes/files/etcd.sh
@@ -4,7 +4,7 @@ etcdctl ls /kube-centos/network 2&> /tmp/etcdctl-tmp.log
 
 if [ "`cat /tmp/etcdctl-tmp.log | grep Error:`" != "" ] ; then 
    etcdctl mkdir /kube-centos/network
-   etcdctl mk /kube-centos/network/config "{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }"
+   etcdctl mk /kube-centos/network/config "{ \"Network\"":" \"172.30.0.0/16\", \"SubnetLen\"":" 24, \"Backend\"":" { \"Type\"":" \"vxlan\" } }"
 fi
 
 rm /tmp/etcdctl-tmp.log
